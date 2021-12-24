6a2ef62b44e163b3764425a4a3146e9bdfd5fd58
Florian Bussmann <florian.bussmann@fh-muenster.de>
2021-12-24 16:50:38 +0100

    Animating Your Player

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,20 +1,47 @@
 import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
 import '../helpers/direction.dart';
 
-class Player extends SpriteComponent with HasGameRef {
+class Player extends SpriteAnimationComponent with HasGameRef {
   Direction direction = Direction.none;
   final double _playerSpeed = 300.0;
+  final double _animationSpeed = 0.15;
+  late final SpriteAnimation _runDownAnimation;
+  late final SpriteAnimation _runLeftAnimation;
+  late final SpriteAnimation _runUpAnimation;
+  late final SpriteAnimation _runRightAnimation;
+  late final SpriteAnimation _standingAnimation;
 
   Player()
       : super(
           size: Vector2.all(50.0),
         );
 
+  Future<void> _loadAnimations() async {
+    final spriteSheet = SpriteSheet(
+      image: await gameRef.images.load('player_spritesheet.png'),
+      srcSize: Vector2(29.0, 32.0),
+    );
+
+    _runDownAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 4);
+
+    _runLeftAnimation =
+        spriteSheet.createAnimation(row: 1, stepTime: _animationSpeed, to: 4);
+
+    _runUpAnimation =
+        spriteSheet.createAnimation(row: 2, stepTime: _animationSpeed, to: 4);
+
+    _runRightAnimation =
+        spriteSheet.createAnimation(row: 3, stepTime: _animationSpeed, to: 4);
+
+    _standingAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 1);
+  }
+
   @override
   Future<void> onLoad() async {
-    super.onLoad();
-    sprite = await gameRef.loadSprite('player.png');
-    position = gameRef.size / 2;
+    _loadAnimations().then((_) => {animation = _standingAnimation});
   }
 
   @override
@@ -42,18 +69,23 @@ class Player extends SpriteComponent with HasGameRef {
   void movePlayer(double delta) {
     switch (direction) {
       case Direction.up:
+        animation = _runUpAnimation;
         moveUp(delta);
         break;
       case Direction.down:
+        animation = _runDownAnimation;
         moveDown(delta);
         break;
       case Direction.left:
+        animation = _runLeftAnimation;
         moveLeft(delta);
         break;
       case Direction.right:
+        animation = _runRightAnimation;
         moveRight(delta);
         break;
       case Direction.none:
+        animation = _standingAnimation;
         break;
     }
   }
