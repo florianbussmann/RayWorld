4acf199290c3e9ab35a215d553b039f76f7f9185
Florian Bussmann <florian.bussmann@fh-muenster.de>
2021-12-24 17:40:18 +0100

    Add Keyboard Input

--- lib/ray_world_game.dart
+++ lib/ray_world_game.dart
@@ -1,13 +1,16 @@
 import 'dart:ui';
 
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'components/player.dart';
 import 'helpers/direction.dart';
 import 'components/world.dart';
 import 'components/world_collidable.dart';
 import 'helpers/map_loader.dart';
 
-class RayWorldGame extends FlameGame with HasCollidables {
+class RayWorldGame extends FlameGame with HasCollidables, KeyboardEvents {
   final Player _player = Player();
   final World _world = World();
 
@@ -31,5 +34,30 @@ class RayWorldGame extends FlameGame with HasCollidables {
 
   void onJoypadDirectionChanged(Direction direction) {
     _player.direction = direction;
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+      RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isKeyDown = event is RawKeyDownEvent;
+    Direction? keyDirection = null;
+
+    if (event.logicalKey == LogicalKeyboardKey.keyA) {
+      keyDirection = Direction.left;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyD) {
+      keyDirection = Direction.right;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyW) {
+      keyDirection = Direction.up;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyS) {
+      keyDirection = Direction.down;
+    }
+
+    if (isKeyDown && keyDirection != null) {
+      _player.direction = keyDirection;
+    } else if (_player.direction == keyDirection) {
+      _player.direction = Direction.none;
+    }
+
+    return super.onKeyEvent(event, keysPressed);
   }
 }
